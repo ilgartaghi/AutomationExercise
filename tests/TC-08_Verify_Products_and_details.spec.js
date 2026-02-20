3dda93a207adcf0dc3939a481061153167fecd9d
Ilgar <ilgartaghi@gmail.com>
2026-02-19 23:07:31 -0500

    TC 8 update

--- tests/TC-08_Verify_Products_and_details.spec.js
+++ tests/TC-08_Verify_Products_and_details.spec.js
@@ -8,10 +8,10 @@
     // 7. Click on 'View Product' of first product
     // 8. User is landed to product detail page
     // 9. Verify that detail detail is visible: product name, category, price, availability, condition, brand
-
+/*
 import { test, expect } from '@playwright/test';
 import { HomePage } from './pages/HomePage';
-import { TestCasesPage } from './pages/TestCasesPage';
+import { ProductsPage } from './pages/ProductsPage';
 
 test ('Contuct Us Form', async ({page}) => {
 
@@ -23,4 +23,72 @@ test ('Contuct Us Form', async ({page}) => {
     // 3. Verify that home page is visible successfully
     await expect(page).toHaveTitle('Automation Exercise');
 
+    const product = new ProductsPage(page);
+
+    // 4. Click on 'Products' button
+    await product.openProduct();
+
+    // 5. Verify user is navigated to ALL PRODUCTS page successfully
+    await expect(page).toHaveTitle('Automation Exercise - All Products');
+
+    // 6. The products list is visible
+    const products = await page.$$("//div[@class='features_items']");
+        for(const product of products)
+    {
+        const productName = await product.textContent();
+        console.log(productName);
+    }
+
+    // 7. Click on 'View Product' of first product
+    await product.openFirstProduct();
+
+    // 8. User is landed to product detail page
+    await expect(page).toHaveTitle('Automation Exercise - Product Details');
+
+    // 9. Verify that detail detail is visible: product name, category, price, availability, condition, brand
+    
+    // Product name (h2 inside product-information)
+    await expect(page.locator("div.product-information h2")).toBeVisible();
+    await expect(page.locator("div.product-information h2")).not.toBeEmpty();
+
+    // Category
+    await expect(page.locator("div.product-information p:has-text('Category:')")).toBeVisible();
+
+    // Price
+    await expect(page.locator("div.product-information span span")).toBeVisible();
+
+    // Availability
+    await expect(page.locator("div.product-information p:has-text('Availability:')")).toBeVisible();
+
+    // Condition
+    await expect(page.locator("div.product-information p:has-text('Condition:')")).toBeVisible();
+
+    // Brand
+    await expect(page.locator("div.product-information p:has-text('Brand:')")).toBeVisible();
+
 })
+
+*/
+import { test, expect } from '@playwright/test';
+import { HomePage } from './pages/HomePage';
+import { ProductsPage } from './pages/ProductsPage';
+
+test('Verify Products and Product Details', async ({ page }) => {
+  const home = new HomePage(page);
+  const products = new ProductsPage(page);
+
+  // 1-2. Open home
+  await home.gotoHomePage();
+  await expect(page).toHaveTitle('Automation Exercise');
+
+  // 5. Go to Products page
+  await products.openProducts();
+  await expect(page).toHaveTitle('Automation Exercise - All Products');
+
+  // 7. Open first product details
+  await products.openFirstProduct();
+  await expect(page).toHaveTitle('Automation Exercise - Product Details');
+
+  // 9. Verify product details visible
+  await products.verifyProductDetailsVisible();
+});
--- tests/pages/ProductsPage.js
+++ tests/pages/ProductsPage.js
@@ -1,7 +1,63 @@
-exports.ProductsPage = class ProductsPage{
+/*
+exports.ProductsPage = class ProductsPage {
 
     constructor(page){
         this.page = page;
-        
+        this.productLink = "//a[@href='/products']"; 
+        this.firstProduct = "//div[@class='col-sm-9 padding-right']//div[2]//div[1]//div[2]//ul[1]//li[1]//a[1]";
+    }
+
+    async openProduct() {
+            await this.page.locator(this.productLink).click();
+        }
+
+    async openFirstProduct() {
+        await this.page.locator(this.firstProduct).click();
     }
 }
+    */
+
+exports.ProductsPage = class ProductsPage {
+  constructor(page) {
+    this.page = page;
+
+    // Top menu "Products"
+    this.productsLink = "a[href='/products']";
+
+    // Products grid
+    this.productsGrid = ".features_items";
+
+    // First product "View Product" link (stable)
+    this.firstViewProductLink = ".features_items a[href^='/product_details/']:has-text('View Product')";
+
+    // Product details block
+    this.productInfo = "div.product-information";
+    this.productName = `${this.productInfo} h2`;
+    this.category = `${this.productInfo} p:has-text('Category:')`;
+    this.price = `${this.productInfo} span span`; // inner span has 'Rs. 500'
+    this.availability = `${this.productInfo} p:has-text('Availability:')`;
+    this.condition = `${this.productInfo} p:has-text('Condition:')`;
+    this.brand = `${this.productInfo} p:has-text('Brand:')`;
+  }
+
+  async openProducts() {
+    await this.page.locator(this.productsLink).click();
+    await this.page.locator(this.productsGrid).waitFor();
+  }
+
+  async openFirstProduct() {
+    await this.page.locator(this.firstViewProductLink).first().click();
+    await this.page.locator(this.productInfo).waitFor();
+  }
+
+  async verifyProductDetailsVisible() {
+    await this.page.locator(this.productInfo).waitFor();
+
+    await this.page.locator(this.productName).isVisible();
+    await this.page.locator(this.category).isVisible();
+    await this.page.locator(this.price).isVisible();
+    await this.page.locator(this.availability).isVisible();
+    await this.page.locator(this.condition).isVisible();
+    await this.page.locator(this.brand).isVisible();
+  }
+};
